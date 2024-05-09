d769369d2b057c23c340db161543a0a9428e873d
d-kraz <daniel.krasny@gmail.com>
2024-05-09 10:16:29 -0500

    progress commit 1

--- script.js
+++ script.js
@@ -2,5 +2,35 @@ function runProgram(){
 
     console.log('runProgram');
     //your code goes here
+    countrySearch();
 }
-document.addEventListener('DOMContentLoaded', runProgram);
+
+function countrySearch(){
+    const countryInputButton = document.querySelector('#search-button');
+    countryInputButton.addEventListener('click', showCountry);
+    
+}
+
+async function showCountry(){
+    const countryInput = document.querySelector('#search-input');
+    const countryInputValue = countryInput.value;
+
+    const apiURL = `https://restcountries.com/v3.1/name/${countryInputValue}`;
+
+    let countryData = null
+    try{
+        const response = await fetch(apiURL);
+        console.log(response);
+        if(response.ok === false){
+            alert("Country not found. Check spelling.")
+        }
+        countryData = await response.json();
+        console.log(countryData);
+    }
+    catch(error){
+        console.log('Error fetching country data', error);
+    }
+}
+
+
+    document.addEventListener('DOMContentLoaded', runProgram);
